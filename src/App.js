a108065489a400acb49281cc88e2cb60436f8138
kishore007k <kishorekumarstar07@gmail.com>
2020-12-28 14:52:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+	display: block;
+	max-width: 1300px;
+	min-height: 100vh;
+	margin: auto;
+
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		margin: 60px auto;
+	}
+
+	h3 {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initialHeight = window.innerHeight;
+	const [currentPosition, setCurrentPosition] = useState(initialHeight);
+	// var currentHeight = window.pageYOffset;
+	// const [lastPosition, setLastPosition] = useState(currentHeight);
+
+	const handleScroll = () => {
+		const Page = document.getElementById("page");
+		const para = document.createElement("div");
+		para.innerHTML = `<h3>Hello World</h3>`;
+		const currentHeight = window.pageYOffset;
+		const totalHeight = currentHeight + initialHeight;
+
+		// if (lastPosition >= currentHeight) {
+		// 	const array = Page.childNodes.length;
+		// 	var number = Math.floor(array);
+		// 	if (number >= 1) {
+		// 		var chil = Page.children;
+		// 		chil[number - 1].style.display = "none";
+		// 		number = number - 1;
+		// 	}
+		// }
+
+		if (totalHeight >= currentPosition) {
+			const y = totalHeight + 1000;
+			setCurrentPosition(y);
+			Page.appendChild(para);
+			// setLastPosition(currentHeight);
+		}
+	};
+
+	window.onscroll = handleScroll;
+	return (
+		<Wrapper id="page">
+			<div>
+				<h3>Hello World</h3>
+			</div>
+		</Wrapper>
+	);
 }
 
 export default App;
